97d0bea8ede2f0774a73c204968aeed96657c0b4
vikas <vikas_tiwari@hcltech.com>
2025-09-18 23:27:00 +0530

    add 2 numbers

--- second.py
+++ second.py
@@ -1,3 +1,6 @@
-a = 100
-b = 100
-print (a+b)
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1+num2
+
+print("The sum of", num1, "and", num2, "is", sum)
